59532087adcd457711bdd62fa6db04226e6361da
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-05-09 09:47:05 +0200

    script functional

---
+++ download_and_set_wallpaper.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import pendulum
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()  # take environment variables from .env.
+
+    day = pendulum.yesterday(tz="Europe/Paris").date().isoformat()
+
+    KEY = os.getenv("KEY")
+
+    url = "https://api.nasa.gov/planetary/apod"
+    params = {"api_key": KEY, "date": day, "thumbs": True}
+
+    response = requests.get(url, params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # download the image
+        if data.get("media_type") == "image":
+            url = data.get("hdurl")
+            dir_name = "/home/phil/Images/wallpapers/NASA/pic_of_the_day/"
+            os.makedirs(dir_name, exist_ok=True)
+            dl_command = f"wget -cq {url} --directory-prefix={dir_name}"
+            os.system(dl_command)
+            # set the wallpaper with feh
+
+            filename = url
+            while "/" in filename:
+                next_slash = filename.index("/")
+                filename = filename[next_slash + 1 :]
+            file_path = dir_name + filename
+            os.system(f"feh --bg-scale {file_path}")
+            print("Wallpaper set.")
+        else:
+            print(f"Today's Pic of the day is a {data.get('media_type')}.")
+
+
+if __name__ == "__main__":
+    main()
